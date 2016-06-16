4e2463d1ed9a96fd86c8cf93c807709f63c9877c
loui123 <e874314@gmail.com>
2016-06-16 07:43:35 +0000

    Add files via upload

--- src/main/java/rest/main.java
+++ src/main/java/rest/main.java
@@ -1,20 +1,11 @@
 package rest;
 
-import java.util.HashMap;
-import java.util.Properties;
-
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 import controller.SimpleController;
 
 public class main {
-
 	public static void main(String[] args) {
-		Properties properties = new Properties();
-		properties.setProperty("java.runtime.version", "1.7");
-		HashMap<String, Object> props = new HashMap<String, Object>();
-		props.put("server.port", 9999);
-
-		new SpringApplicationBuilder().sources(SimpleController.class).properties(props).run(args);
+		new SpringApplicationBuilder().sources(SimpleController.class).run(args);
 	}
 }
